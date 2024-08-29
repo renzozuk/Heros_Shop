c756c99fe94106cb2428208f361253f1fb36b736
renzozuk <renzo.zuk19@gmail.com>
2024-08-29 19:28:02 -0300

    Backend: Spring project initialized

---
+++ backend/Heros_Shop_API/src/main/java/com/heros_shop/heros_shop_api/HerosShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.heros_shop.heros_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerosShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerosShopApiApplication.class, args);
+    }
+
+}
---
+++ backend/Heros_Shop_API/src/test/java/com/heros_shop/heros_shop_api/HerosShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heros_shop.heros_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerosShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
